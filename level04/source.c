b3efb0d68e19ac718075c5b876e0bb92b176b5bb
Ludovic DEDIER <ldedier@e3r3p15.42.fr>
2020-01-15 21:18:42 +0100

    refined source 4

--- level04/source.c
+++ level04/source.c
@@ -6,34 +6,41 @@
 /*   By: ldedier <ldedier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 18:24:33 by ldedier           #+#    #+#             */
-/*   Updated: 2020/01/15 18:55:41 by ldedier          ###   ########.fr       */
+/*   Updated: 2020/01/15 21:16:49 by ldedier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 int main(int argc, char **argv)
 {
-	char buffer[176];
-	pid_t child;
+	char	buffer[176];
+	pid_t	child;
+
 	child_pid = fork();
 	bzero(buffer + 32, 128); //-> 160
 	buffer[28] = 0;
 	buffer[168] = 0;
-
 	if (child_pid) //parent
 	{
+parent_beginning:
 		wait(buffer + 28);
 		buffer[160] = *(buffer + 28);
-		if (WTERMSIG(buffer[160]))
+		if (WTERMSIG(buffer[160]) == 0 || WIFSIGNALED(buffer[160]) > 0) 
 		{
-			if (WIFSIGNALED(buffer[160]))
-			{
-
-			}
+			// WTERMSIG returns the numeric value of the signal that was raised by the process
+			// WIFSIGNALED indicates that the child process exited because it raised a signal
+			puts("child is exiting...");
 		}
 		else
 		{
-
+			res = ptrace(3, child_pid, 44, 0);
+			buffer[168] = res;
+			if (res == 11)
+			{
+				puts("no exec() for you");
+				kill(child_pid, 9);
+			}
+			else
+				goto parent_beginning;
 		}
 	}
 	else //child
