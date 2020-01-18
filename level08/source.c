f8d6ccaf50ba0a9473c75c28bf3973741d640418
Ludovic DEDIER <ldedier@e3r3p16.42.fr>
2020-01-18 16:55:04 +0100

    added walkthrough + ended source level 8

--- level08/source.c
+++ level08/source.c
@@ -6,7 +6,7 @@
 /*   By: ldedier <ldedier@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/18 11:17:24 by ldedier           #+#    #+#             */
-/*   Updated: 2020/01/18 16:03:38 by ldedier          ###   ########.fr       */
+/*   Updated: 2020/01/18 16:41:35 by ldedier          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,31 +39,32 @@ int main(int argc, char **argv)
 	FILE	*readfile
 
 	if (argc != 2)
-	{
 		printf("Usage: %s filename\n", *argv);
-		if ((writefile = fopen("./backups/.log", "w")) == NULL) // - 0x88
-		{
-			printf("ERROR: Failed to open %s\n", "./backups/.log");
-			exit(1);
-		}
-
-		log_wrapper(writefile, "Starting back up: ", argv[1]);
-//+195
-		if ((readFile = fopen(argv[1], "r")) == NULL) // - 0x80
-		{
-			printf("ERROR: Failed to open %s\n", argv[1]);
-			exit(1);
-		}
-
-//+281
-
-		buffer[64] = "./backups/"; // - 0x70
-
-		strncat(buffer + 64, argv[1], 99 - strlen(buffer + 64));
-//+311
-
-		
-
+	if ((writefile = fopen("./backups/.log", "w")) == NULL)
+	{
+		printf("ERROR: Failed to open %s\n", "./backups/.log");
+		exit(1);
 	}
+	log_wrapper(writefile, "Starting back up: ", argv[1]);
+	if ((readFile = fopen(argv[1], "r")) == NULL)
+	{
+		printf("ERROR: Failed to open %s\n", argv[1]);
+		exit(1);
+	}
+	buffer[64] = "./backups/";
+	strncat(buffer + 64, argv[1], 99 - strlen(buffer + 64));
+	if ((fd = open(buffer + 64, O_CREAT | O_WRITE)) == -1)
+	{
+		printf("ERROR: Failed to open %s%s\n", "./backups/", argv[1]);
+		exit(1);
+	}
+
+	while ((ret = fgetc(readfile)) != -1)
+		write(fd, &ret, 1);
+
+	log_wrapper(writefile, "Finished back up: ", argv[1]);
+	fclose(readfile);
+	close(fd);
+	//check_canary();
 	return (0);
 }
